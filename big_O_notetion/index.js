a5fe9afd6d86f5fa3779fbf7620d08dccec84833
md kawsar alam <mdkawsaralam537@gmail.com>
2024-08-19 22:37:14 +0600

    use while loop

--- big_O_notetion/index.js
+++ big_O_notetion/index.js
@@ -8,13 +8,45 @@
 // }
 
 // console.log(sum([23,8,6]));
- const double=(arr) =>{
-     let newArray=[];
-      for (let i = 0; i < arr.length; i++) {
-        newArray.push(arr[i]*2);
+
+//  const double=(arr) =>{
+//      let newArray=[];
+//       for (let i = 0; i < arr.length; i++) {
+//         newArray.push(arr[i]*2);
         
-      }
-      return newArray
- }
- console.log(double([25,30,40]));
+//       }
+//       return newArray
+//  }
+//  console.log(double([25,30,40]));
  
+
+// time and space complexity
+
+//  const uniqName= (arr)=>{
+//      let uniqNames=[];
+//      for (let i = 0; i < arr.length; i++) {
+//         let element = arr[i];
+//          if (!uniqNames.includes(element)) {
+//             uniqNames.push(element)
+
+////time complexity==n2 and space complexity=n
+            
+//          }
+        
+//      }
+//      return  uniqNames
+//  }
+//   console.log(uniqName(['kawsar', 'raju','asad','kawsar']));
+  
+
+//  use while loop
+     const  logarithmN=(n)=>{
+        while (n>1) {
+             console.log(n)
+             n/=2;
+            
+        }
+        console.log('done');
+        
+     }
+      logarithmN(25);
